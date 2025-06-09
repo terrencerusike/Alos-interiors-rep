0657b90b3bb8ffc683c5d93db0f1a00d8ac2895e
ImTerry <154828561+terrencerusike@users.noreply.github.com>
2025-06-09 16:12:07 +0200

    c

--- src/components/CatalogSection.jsx
+++ src/components/CatalogSection.jsx
@@ -12,10 +12,10 @@ function CatalogSection() {
   return (
     <div className="catalog-section">
       <div className="grid">
-        {categories[5] && (
+        {categories[12] && (
           <div className="item item1">
             <Link to={`/Shop/${categories[5].id}`}>
-              <p>{categories[5].name}</p>
+              <p>{categories[12].name}</p>
             </Link>
           </div>
         )}
